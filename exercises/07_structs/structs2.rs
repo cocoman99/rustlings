6c80409f39ca3c3ab909c7559a2ed0c6a57a3756
cocoman99 <cocoman99@o2.pl>
2024-09-19 17:22:24 +0200

    Update structs2.rs

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -13,11 +13,11 @@ fn create_order_template() -> Order {
     Order {
         name: String::from("Bob"),
         year: 2019,
-        made_by_phone: false,
-        made_by_mobile: false,
-        made_by_email: true,
-        item_number: 123,
-        count: 0,
+        made_by_phone: true,
+        made_by_mobile: true,
+        made_by_email: false,
+        item_number: 321,
+        count: 10,
     }
 }
 
@@ -33,15 +33,23 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
 
-        // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        
+        let your_order = Order { 
+            name: String::from("CoCo"),
+            year: 2019,
+            made_by_phone: true,
+            made_by_mobile: true,
+            made_by_email: false,
+            item_number: 321,
+            count: 10,
+         };
 
-        assert_eq!(your_order.name, "Hacker in Rust");
+        assert_eq!(your_order.name, "CoCo");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
         assert_eq!(your_order.made_by_mobile, order_template.made_by_mobile);
         assert_eq!(your_order.made_by_email, order_template.made_by_email);
         assert_eq!(your_order.item_number, order_template.item_number);
-        assert_eq!(your_order.count, 1);
+        assert_eq!(your_order.count, 10);
     }
 }
